56f87065855b01763cd3380736ab53102a958da2
Jake <jxv3386@rit.edu>
2021-10-06 14:41:09 -0400

    get data-tracking properties

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 import yaml
 
 options = webdriver.ChromeOptions()
-options.add_argument("headless")
+#options.add_argument("headless")
 driver = webdriver.Chrome(options=options)
 
 
@@ -21,10 +21,17 @@ def openYaml(filePath):
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-def parseTable(soup):
-    rows=soup.find_all("tr")
-    for i in rows:
-        print(i)
+def parseTable(table):
+    soup = BeautifulSoup(table, "html.parser").find('table')
+
+    pd.set_option('display.max_columns', 100)
+    pd.set_option('display.width', 1000)
+    dtp = soup.find_all("td", class_="athlete track-click")
+    for i in dtp:
+        print(i.attrs.get("data-tracking-properties"))
+        print("==============================")
+    df = pd.read_html(str(soup))[0]
+    print(df)
 def parseRow(row):
     return row
 def main():
@@ -33,15 +40,11 @@ def main():
     myStPassword = conf['strava_user']['password']
     login("https://www.strava.com/login", "email", myStEmail, "password", myStPassword, "login-button")
     driver.get("https://www.strava.com/segments/17747336")
-    results=driver.find_element_by_id("results").get_attribute('innerHTML')
-    table = BeautifulSoup(results, "html.parser").find('table')
 
-    pd.set_option('display.max_columns', 100)
-    pd.set_option('display.width', 1000)
-    df = pd.read_html(str(table))[0]
-    print(df)
-    #parseTable(df)
-    driver.close()
+    table=driver.find_element_by_id("results").get_attribute('innerHTML')
+    parseTable(table)
+
+   # driver.close()
 if __name__ == '__main__':
     main()
 
