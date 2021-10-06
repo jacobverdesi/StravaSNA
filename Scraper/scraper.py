788d63ef8968313038f9997ace402662b2cfded2
Jake <jxv3386@rit.edu>
2021-10-06 11:27:58 -0400

    parse with pandas read_html

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
 import yaml
 
 options = webdriver.ChromeOptions()
-#options.add_argument("headless")
+options.add_argument("headless")
 driver = webdriver.Chrome(options=options)
 
 
@@ -19,17 +21,27 @@ def openYaml(filePath):
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-
-
+def parseTable(soup):
+    rows=soup.find_all("tr")
+    for i in rows:
+        print(i)
+def parseRow(row):
+    return row
 def main():
     conf = openYaml('login.yaml')
     myStEmail = conf['strava_user']['email']
     myStPassword = conf['strava_user']['password']
     login("https://www.strava.com/login", "email", myStEmail, "password", myStPassword, "login-button")
-    driver.get("https://www.strava.com/segments/9846518")
-    table=driver.find_element_by_id("results").get_attribute('innerHTML')
-    print(table)
+    driver.get("https://www.strava.com/segments/17747336")
+    results=driver.find_element_by_id("results").get_attribute('innerHTML')
+    table = BeautifulSoup(results, "html.parser").find('table')
 
+    pd.set_option('display.max_columns', 100)
+    pd.set_option('display.width', 1000)
+    df = pd.read_html(str(table))[0]
+    print(df)
+    #parseTable(df)
+    driver.close()
 if __name__ == '__main__':
     main()
 
