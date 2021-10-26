9c7a3f560d23e42028565b584a90c1e5b21f1243
Jake <jxv3386@rit.edu>
2021-10-26 13:03:27 -0400

    commit

--- Scraper/StravaScraper.py
+++ Scraper/StravaScraper.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
 from bs4 import BeautifulSoup
 from datetime import timedelta
 import pandas as pd
@@ -10,7 +12,6 @@ import yaml
 import json
 import ast
 from pathlib import Path
-
 
 BASE_URL = "https://www.strava.com"
 
@@ -20,23 +21,25 @@ URL_DASHBOARD = "%s/dashboard" % BASE_URL
 
 SEGMENT_SLEEP = 5
 
-def login(driver,url, usernameId, username, passwordId, password, submit_buttonId):
+
+def login(driver, url, usernameId, username, passwordId, password, submit_buttonId):
     """
     driver function to login to a login page
     directs to url finds username and password fields and enters data
     then finds submit id and clicks
     """
     driver.get(url)
-    driver.find_element(By.XPATH,f'//input[@id=\"{usernameId}\"]').send_keys(username)
-    driver.find_element(By.XPATH,f'//input[@id=\"{passwordId}\"]').send_keys(password)
-    driver.find_element(By.XPATH,f'//button[@id=\"{submit_buttonId}\"]').click()
+    driver.find_element(By.XPATH, f'//input[@id=\"{usernameId}\"]').send_keys(username)
+    driver.find_element(By.XPATH, f'//input[@id=\"{passwordId}\"]').send_keys(password)
+    driver.find_element(By.XPATH, f'//button[@id=\"{submit_buttonId}\"]').click()
     driver.implicitly_wait(5)
     strUrl = driver.current_url
-    if strUrl !="https://www.strava.com/login":
+    if strUrl != "https://www.strava.com/login":
         print(f"Logged in as: {username}")
         return True
     print(f"Failed to Log in as: {username}")
     return False
+
 
 def openYaml(filePath):
     """
@@ -49,7 +52,9 @@ def openYaml(filePath):
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-def dumpYaml(filePath,data):
+
+
+def dumpYaml(filePath, data):
     """
     dump yaml file and return datastream
     :param filePath:
@@ -57,6 +62,7 @@ def dumpYaml(filePath,data):
     """
     with open(filePath, "w") as stream:
         return yaml.dump(data, stream)
+
 
 def parseTable(table):
     """
@@ -97,9 +103,9 @@ def progress(percent=0, width=30):
     print('\r[', '#' * left, ' ' * right, ']', f' {percent:.0f}%', sep='', end='', flush=True)
 
 
-def getSegment(driver,id):
+def getSegment(driver, id):
     driver.get(URL_SEGMENT + id)
-    ended=False
+    ended = False
     df = pd.DataFrame()
     active_page = 1
     max = 0
@@ -108,34 +114,39 @@ def getSegment(driver,id):
 
         try:
 
-            WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH,'//div[@id="results"]'))) # Wait for table to load
-            table = driver.find_element(By.XPATH,'//div[@id="results"]').get_attribute('innerHTML') # Get the table
-            df = df.append(parseTable(table), ignore_index=True)    # Add to dataframe
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, '//div[@id="results"]')))  # Wait for table to load
+            table = driver.find_element(By.XPATH, '//div[@id="results"]').get_attribute('innerHTML')  # Get the table
+            df = df.append(parseTable(table), ignore_index=True)  # Add to dataframe
 
 
-        except Exception as e:
-            print()
-            if (driver.find_element(By.XPATH, '//div[@class="alert alert-warning mt-md"]')>0):
+        except TimeoutException:
+            try:
+                driver.find_element(By.XPATH, '//div[@class="alert alert-warning mt-md"]')
                 print("Hazardous segment skipping")
                 break
-            print(f"Error trying to get table of segment: {id} on page {active_page}")
-            return False , active_page
+            except NoSuchElementException:
+
+                print(f"Error trying to get table of segment: {id} on page {active_page}")
+                return False, active_page
 
         if (active_page == 1):
             # Get total number of pages break if only one page
             try:
-                max = int(driver.find_element(By.XPATH, '//ul[@class="pagination"]/li[last()-1]/a').get_attribute('innerHTML'))
+                max = int(driver.find_element(By.XPATH, '//ul[@class="pagination"]/li[last()-1]/a').get_attribute(
+                    'innerHTML'))
             except:
                 break
 
         progress(percent=int((active_page / max) * 100))
-        active_page +=1
+        active_page += 1
 
         try:
-            if(driver.find_element(By.XPATH,'//ul[@class="pagination"]/li[last()]').get_attribute("class")=="next_page disabled"):
-
+            if (driver.find_element(By.XPATH, '//ul[@class="pagination"]/li[last()]').get_attribute(
+                    "class") == "next_page disabled"):
                 break
-            WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '//li[@class="next_page"]/a[@rel="next"]')))
+            WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.XPATH, '//li[@class="next_page"]/a[@rel="next"]')))
             driver.find_element(By.XPATH, '//li[@class="next_page"]/a[@rel="next"]').click()
             WebDriverWait(driver, 3).until(
                 EC.text_to_be_present_in_element(
@@ -147,40 +158,44 @@ def getSegment(driver,id):
                 print()
                 print("Max requests today")
                 return False, active_page
+    print()
+    if (len(df.index) > 0):
+        df.to_csv(f"./Data/Master/Segments/{str(id)}.csv", index=False)
+        print(df)
+    return True, active_page
 
-    print(df.size)
-    if(df.shape[0]>0):
-        df.to_csv(f"../Data/Master/Segments/{str(id)}.csv", index=False)
-    print(df)
-    return True , active_page
+
 def getAllSegmentsFromDict(driver):
     base_path = Path(__file__).parent
     file_path = (base_path / "../Data/Master/segmentList.txt").resolve()
-    totalPages=0
-    totalErrors=0
+    totalPages = 0
+    totalErrors = 0
     with open(file_path, 'r') as f:
         my_set = dict(ast.literal_eval(f.read()))
     try:
         for i in my_set:
-            print(f"{i} {my_set[i]}")
             if not my_set[i]:
-                succeeded,pages=getSegment(driver,i)
-                my_set[i]=succeeded
-                totalErrors+=int(not succeeded)
-                totalPages+=pages
+                # print(f"{i} {my_set[i]}")
+                succeeded, pages = getSegment(driver, i)
+                my_set[i] = succeeded
+                totalErrors += int(not succeeded)
+                totalPages += pages
                 print(f"Made {pages} requests for total of {totalPages}")
-                if totalErrors>2:
+                if totalErrors > 2:
                     raise Exception
                 print("Sleeping")
                 for i in range(SEGMENT_SLEEP):
                     progress(int(i / SEGMENT_SLEEP * 100))
                     time.sleep(1)
                 print()
-    except:
+    except Exception as e:
+        print(e)
         with open(file_path, 'w') as f:
             f.write(str(my_set))
             print("\nUpdated segmentList.txt")
-    return totalErrors>2
+    return totalErrors > 2
+
+
 def main():
     pd.set_option('display.max_columns', 100)
     pd.set_option('display.width', 1000)
@@ -191,24 +206,26 @@ def main():
     options.add_argument("--allow-running-insecure-content")
     # options.add_argument("--user-data-dir=C:\\Users\\Jake\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 2")
 
-    #options.add_argument("headless")
+    options.add_argument("headless")
 
     for user in conf:
-        myStEmail=conf[user]['email']
-        myStPassword=conf[user]['password']
+        myStEmail = conf[user]['email']
+        myStPassword = conf[user]['password']
 
-        if(time.time()-conf[user]['lastmaxed']>86400):
+        if (time.time() - conf[user]['lastmaxed'] > 86400):
             driver = webdriver.Chrome(options=options)
-            succeeded=login(driver,URL_LOGIN, "email", myStEmail, "password", myStPassword, "login-button")
+            succeeded = login(driver, URL_LOGIN, "email", myStEmail, "password", myStPassword, "login-button")
             if succeeded:
-                succeeded=getAllSegmentsFromDict(driver)
+                succeeded = getAllSegmentsFromDict(driver)
             if succeeded:
-                conf[user]['lastmaxed']=time.time()
-                dumpYaml('Scraper/login.yaml',conf)
+                conf[user]['lastmaxed'] = time.time()
+                dumpYaml('Scraper/login.yaml', conf)
 
             driver.close()
         else:
-            print(f"{myStEmail} is probably timedout {str(timedelta(seconds=86400-(time.time()-conf[user]['lastmaxed'])))} remaining ")
+            print(
+                f"{myStEmail} is probably timedout {str(timedelta(seconds=86400 - (time.time() - conf[user]['lastmaxed'])))} remaining ")
+
 
 if __name__ == '__main__':
     main()
