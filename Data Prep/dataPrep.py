030d2f3936d5262abcb456f96902827c6dda98cb
Jake <jxv3386@rit.edu>
2021-10-28 02:31:22 -0400

    created egocentric network in dataPrep.py

--- Data Prep/dataPrep.py
+++ Data Prep/dataPrep.py
@@ -3,6 +3,8 @@ import math
 from os import listdir
 from os.path import join, isfile
 from pathlib import Path
+import random
+
 import pandas as pd
 from itertools import combinations
 
@@ -32,62 +34,119 @@ def createFullNetwork():
     file_path = (base_path / "../Data/Master/Segments").resolve()
     currentSegments = sorted([int(f[:-4]) for f in listdir(file_path) if isfile(join(file_path, f))])
     print("Total segments: ",len(currentSegments))
-    masterDict={}
+    network={}
     count=0
     for segment in currentSegments:
         count+=1
         file_path = (base_path / f"../Data/Master/Segments/{segment}.csv").resolve()
         athlete_list = combinations(pd.read_csv(file_path)['athlete_id'], 2)
         for i in list(athlete_list):
-            if i in masterDict.keys():
-                masterDict[i] += 1
-            elif i[::-1] in masterDict.keys():
-                masterDict[i[::-1]] += 1
+            if i in network.keys():
+                network[i] += 1
+            elif i[::-1] in network.keys():
+                network[i[::-1]] += 1
             else:
-                masterDict[i] = 1
+                network[i] = 1
         progress(count,len(currentSegments))
     print()
-    network = pd.Series(masterDict).reset_index()
+    network = pd.Series(network).reset_index()
     print("Network size: ",len(network.index))
 
     network.columns = ["Source", "Target", "weight"]
-    network.to_csv(f"../Data/Networks/fullNetwork.csv")
+    network.to_csv(f"../Data/Networks/fullNetwork.csv",index=False)
     return network
 
 def getFullNetwork(new=False):
+    """
+    Helper to get FullNetwork
+    if new=False then look for it in Data/Network else createFullNetwork
+    :param new:
+    :return:
+    """
     base_path = Path(__file__).parent
     if(new):
         return createFullNetwork()
     else:
         return  pd.read_csv((base_path / f"../Data/Networks/fullNetwork.csv").resolve())
 
-def createEgoNetwork(seed, n, neighbors=None):
-    if neighbors is None:
-        neighbors = set()
+def createEgoNetwork(seed, n,visited=None):
+    if visited == None:
+        visited=[seed]
+    else:
+        visited.append(seed)
     base_path = Path(__file__).parent
     file_path = (base_path / "../Data/Master/Segments").resolve()
     currentSegments = [int(f[:-4]) for f in listdir(file_path) if isfile(join(file_path, f))]
-    print(len(currentSegments))
-    count=0
+    network={}
+    print("Seed: ",seed)
+    neighbors=set()
     for segment in currentSegments:
         file_path = (base_path / f"../Data/Master/Segments/{segment}.csv").resolve()
         segmentDf=pd.read_csv(file_path)
 
         if(seed in segmentDf['athlete_id'].unique()):
-            neighbors.update(segmentDf['athlete_id'].unique())
-        #count += int(seed in segmentDf['athlete_id'].unique())
-        #print(neighbors)
-        #athlete_list = combinations(pd.read_csv(file_path)['athlete_id'], 2)
-       # print(segmentDf)
+            neighbors.update(set(segmentDf['athlete_id']))
+
     neighbors.remove(seed)
 
-    print(len(neighbors))
-#     return df
+    neighborCount=0
+    neighbors=random.sample(neighbors,10**n)
+    for neighbor in neighbors:
+        neighborCount+=1
+        pair=(seed,neighbor)
+        if pair in network.keys():
+            network[pair] += 1
+        elif pair[::-1] in network.keys():
+            network[pair[::-1]] += 1
+        else:
+            network[pair] = 1
 
+        if n>0 and neighbor not in visited:
+            subnet=createEgoNetwork(neighbor,n-1,visited)
+            print(subnet)
+            for key in subnet.keys():
+                if key in network.keys():
+                    network[key]+= subnet[key]
+                elif key[::-1] in network.keys():
+                    network[key[::-1]]+= subnet[key]
+                else:
+                    network[key] = subnet[key]
+
+            print(f"N:{n} Neighborcount: {neighborCount}/{len(neighbors)}")
+
+
+    return network
+
+def createEgoNetwork2(network,seed ,n):
+    newNetwork=network.loc[(network['Target'] == seed) | (network['Source'] == seed )].sample(100)
+
+    network=network.drop(newNetwork.index).reset_index(drop=True)
+    seeds=set(newNetwork['Target'].unique().tolist()+newNetwork['Source'].unique().tolist())
+    seeds.remove(seed)
+    if n>0:
+
+        for idx,newSeed in enumerate(seeds):
+            print(f"N: {n} Seed num{idx}/{len(seeds)}")
+            newNetwork=newNetwork.append(createEgoNetwork2(network, newSeed, n-1))
+    return newNetwork
+
+def getEgoNetwork(seed,n,new=False):
+    base_path = Path(__file__).parent
+    if (new):
+        network = getFullNetwork()
+        network=createEgoNetwork2(network,seed,n)
+        # network = pd.Series(network).reset_index()
+        # print("Network size: ", len(network.index))
+        #
+        # network.columns = ["Source", "Target", "weight"]
+        network.to_csv(f"../Data/Networks/{seed}_egoNetwork_{n}.csv",index=False)
+        return network
+    else:
+        return pd.read_csv((base_path / f"../Data/Networks/{seed}_egoNetwork_{n}.csv").resolve())
 
 def main():
-    #createEgoNetwork(45406272,2)
-    network=getFullNetwork(True)
+    getEgoNetwork(45406272,1,True)
+    #network=getFullNetwork(True)
     # print(network)
     # network=network.loc[network['weight'] > 1]
     # print(network)
